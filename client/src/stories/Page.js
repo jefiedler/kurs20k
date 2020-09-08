572d149ce2f84a58475b8eb08670409d955c993c
jefiedler <jonas.fiedler@magenta.de>
2020-09-08 16:28:03 +0200

    Fix some problems

--- client/src/stories/Page.js
+++ client/src/stories/Page.js
@@ -1,8 +1,8 @@
 import React from "react";
 import PropTypes from "prop-types";
 
-import Header from "../../components/Header";
 import "./page.css";
+import { Header } from "./Header";
 
 const Page = ({ user, onLogin, onLogout, onCreateAccount }) => (
   <article>
