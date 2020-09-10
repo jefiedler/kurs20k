8a911548b2a7b8cd3c237ddaf8d8e79ef0f37591
jefiedler <jonas.fiedler@magenta.de>
2020-09-10 14:57:40 +0200

    Generate fake participants and save in MongoDB

--- generateFakeParticipants.js
+++ generateFakeParticipants.js
@@ -1,5 +1,10 @@
 require("dotenv").config();
 const faker = require("faker/locale/de");
+const { MongoClient } = require("mongodb");
+
+const dbclient = new MongoClient(process.env.MONGO_URI, {
+  useUnifiedTopology: true,
+});
 
 function getRandomeBirth() {
   const day = Math.floor(Math.random() * (31 - 1)) + 1;
@@ -10,28 +15,35 @@ function getRandomeBirth() {
   return birthDate;
 }
 
-function generateFakeParticipants() {
-  let participant = {};
+async function generateFakeParticipants(startValue, generateValue) {
+  try {
+    await dbclient.connect();
+    const database = dbclient.db(process.env.MONGO_DB_NAME);
+    const collection = database.collection("participants");
 
-  for (let index = 0; index <= 10; index++) {
-    let participant = {
-      firstName: faker.name.firstName(),
-      lastName: faker.name.lastName(),
-      birthdDate: getRandomeBirth(),
-      street:
-        faker.address.streetName() +
-        " " +
-        Math.floor(Math.random() * (1000 - 1)),
-      zipCode: faker.address.zipCode(),
-      city: faker.address.city(),
-      country: faker.address.country(),
-      email: faker.internet.email(),
-      landline: faker.phone.phoneNumber(),
-      mobile: faker.phone.phoneNumber(),
-    };
-    console.log(participant);
+    for (let index = startValue; index <= generateValue; index++) {
+      let participant = {
+        firstName: faker.name.firstName(),
+        lastName: faker.name.lastName(),
+        birthDate: getRandomeBirth(),
+        street:
+          faker.address.streetName() +
+          " " +
+          Math.floor(Math.random() * (1000 - 1)),
+        zipCode: faker.address.zipCode(),
+        city: faker.address.city(),
+        country: faker.address.country(),
+        email: faker.internet.email(),
+        landline: faker.phone.phoneNumber(),
+        mobile: faker.phone.phoneNumber(),
+      };
+      await collection.insertOne(participant);
+    }
+  } catch (error) {
+    //Ensures that the client will close when you finish/error
+    await dbclient.close();
+    console.error(error);
   }
-  return participant;
 }
 
-generateFakeParticipants();
+generateFakeParticipants(0, 99);
