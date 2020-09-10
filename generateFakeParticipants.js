2e223a9312f5bc251e7aa7bd06dabaa62dd0ac59
jefiedler <jonas.fiedler@magenta.de>
2020-09-10 12:28:14 +0200

    Finish function for generate fakeData

--- generateFakeParticipants.js
+++ generateFakeParticipants.js
@@ -1,6 +1,14 @@
 require("dotenv").config();
-const faker = require("faker");
-const { fake } = require("faker");
+const faker = require("faker/locale/de");
+
+function getRandomeBirth() {
+  const day = Math.floor(Math.random() * (31 - 1)) + 1;
+  const month = Math.floor(Math.random() * (12 - 1)) + 1;
+  const year = Math.floor(Math.random() * (2002 - 1920)) + 1920;
+  const birthDate = `${day}.${month}.${year}`;
+
+  return birthDate;
+}
 
 function generateFakeParticipants() {
   let participant = {};
@@ -9,7 +17,11 @@ function generateFakeParticipants() {
     let participant = {
       firstName: faker.name.firstName(),
       lastName: faker.name.lastName(),
-      street: faker.address.streetName() + " " + index,
+      birthdDate: getRandomeBirth(),
+      street:
+        faker.address.streetName() +
+        " " +
+        Math.floor(Math.random() * (1000 - 1)),
       zipCode: faker.address.zipCode(),
       city: faker.address.city(),
       country: faker.address.country(),
