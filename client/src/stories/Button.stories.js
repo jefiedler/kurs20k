9b511a5f25c9fb6496886b41a8b9feff99e9939e
jefiedler <jonas.fiedler@magenta.de>
2020-09-18 09:54:36 +0200

    Move to button folder in components folder

--- client/src/stories/Button.stories.js
+++ client/src/stories/Button.stories.js
@@ -1,9 +1,9 @@
 import React from "react";
 
-import Button from "./Button";
+import Button from "../components/Buttons/Button";
 
 export default {
-  title: "Example/Button",
+  title: "Example/Buttons/Button",
   component: Button,
   argTypes: {
     backgroundColor: { control: "color" },
