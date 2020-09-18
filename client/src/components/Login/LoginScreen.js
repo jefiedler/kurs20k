31d170279e128a4a8b35b59b9e7069926c1430cb
jefiedler <jonas.fiedler@magenta.de>
2020-09-18 13:14:22 +0200

    Chance layout

--- client/src/components/Login/LoginScreen.js
+++ client/src/components/Login/LoginScreen.js
@@ -7,21 +7,24 @@ import InputField from "../InputFields/InputField";
 function LoginScreen() {
   return (
     <GridContainer>
-      <InputField
-        labelText="Nutzername"
-        inputType="text"
-        placeholder="Username"
-      />
-      <InputField
-        labelText="Passwort"
-        inputType="password"
-        placeholder="Passwort"
-      />
-      <LoginButton label="Login" />
-      <LinkContainer>
-        <Links text="Passwort vergessen?" />
-        <Links text="Account erstellen" />
-      </LinkContainer>
+      <InputContainer>
+        <InputField
+          labelText="Nutzername"
+          inputType="text"
+          placeholder="Username"
+        />
+        <InputField
+          labelText="Passwort"
+          inputType="password"
+          placeholder="Passwort"
+        />
+        <LoginButton buttonLabel="Login" />
+        <LinkContainer>
+          <Links text="Passwort vergessen?" />
+          <Links text="Account erstellen" />
+        </LinkContainer>
+      </InputContainer>
+      <Image src="" alt="Green Snake" />
     </GridContainer>
   );
 }
@@ -32,8 +35,8 @@ export default LoginScreen;
 
 const GridContainer = styled.div`
   display: grid;
-  grid-row: 1fr 1fr;
-  grid-column: auto;
+  grid-template-rows: 1fr;
+  grid-template-columns: 1fr 1fr;
   place-items: center;
   background-color: #3f5941;
 `;
@@ -42,4 +45,13 @@ const LinkContainer = styled.div`
   display: flex;
   justify-content: space-around;
   background-color: #3f5941;
+`;
+
+const InputContainer = styled.div`
+  grid-column: 2/3;
+  grid-row: 1/2;
+`;
+const Image = styled.img`
+  width: 387px;
+  height: 446px;
 `;
