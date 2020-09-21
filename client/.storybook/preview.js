88be8db03746add3a6f1c1a2118e96478a4dc0b9
jefiedler <jonas.fiedler@magenta.de>
2020-09-21 12:29:38 +0200

    Set Global font-family, box-sizing and background-color

--- client/.storybook/preview.js
+++ client/.storybook/preview.js
@@ -1,3 +1,14 @@
+import React from "react";
+import GlobalStyles from "../src/GlobalStyles";
+
 export const parameters = {
   actions: { argTypesRegex: "^on[A-Z].*" },
 };
+
+const AppDecorator = (Story, context) => (
+  <>
+    <GlobalStyles />
+    <Story {...context} />
+  </>
+);
+export const decorators = [AppDecorator];
--- client/src/GlobalStyles.js
+++ client/src/GlobalStyles.js
@@ -3,14 +3,25 @@ import { Global, css } from "@emotion/core";
 
 const GlobalStyles = () => {
   return (
-    <Global>
-      styles=
-      {css`
+    <Global
+      styles={css`
+        @import url("https://fonts.googleapis.com/css2?family=Roboto:ital@0;1&display=swap");
+
+        *,
+        *:before,
+        *:after {
+          box-sizing: border-box;
+        }
+
         html {
-          @import url("https://fonts.googleapis.com/css2?family=Roboto:ital,wght@0,100;0,300;0,400;0,500;0,700;0,900;1,100;1,300;1,400;1,500;1,700;1,900&display=swap");
+        }
+        body {
+          background-color: #f2f2f2;
+          font-family: "Roboto", sans-serif;
+          margin: 0;
         }
       `}
-    </Global>
+    />
   );
 };
 
