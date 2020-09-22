ad9878c431622d76bf03e6c1abe61c2adc83db40
jefiedler <jonas.fiedler@magenta.de>
2020-09-22 11:19:06 +0200

    Add NavItems

---
+++ client/src/stories/Component/NavBar/NavBarItem.js
@@ -0,0 +1,33 @@
+import React from "react";
+import PropTypes from "prop-types";
+import styled from "@emotion/styled";
+
+NavBarItem.propTypes = {
+  label: PropTypes.string,
+  href: PropTypes.string,
+  srcPath: PropTypes.string,
+  imgName: PropTypes.string,
+};
+
+function NavBarItem({ label, href, srcPath, imgName }) {
+  return (
+    <ListItem>
+      <img src={srcPath} alt={imgName} />
+      <a href={href}>{label}</a>
+    </ListItem>
+  );
+}
+
+export default NavBarItem;
+
+//Styling
+
+const ListItem = styled.li`
+  display: flex;
+  list-style: none;
+  align-items: center;
+  a {
+    margin: 0 0 0 10px;
+    color: #d9cc8b;
+  }
+`;
