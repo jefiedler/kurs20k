7897d434a310844fa1bfba2d7c59be4cba53498a
jefiedler <jonas.fiedler@magenta.de>
2020-09-24 14:48:33 +0200

    Add BasicButton component

---
+++ client/src/stories/Component/Buttons/BasicButton.js
@@ -0,0 +1,10 @@
+import React from "react";
+import PropTypes from "prop-types";
+
+BasicButton.propTypes = {};
+
+function BasicButton(props) {
+  return <div></div>;
+}
+
+export default BasicButton;
