ad0cd0e2f27ba40e7f6ff8cf795946e3e7565446
jefiedler <jonas.fiedler@magenta.de>
2020-09-25 18:01:09 +0200

    Add input components to app.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,22 @@
 import React from "react";
+import BasicTextInput from "../src/stories/Component/Inputs/BasicTextInput";
+import DatePickerInput from "../src/stories/Component/Inputs/DatePickerInput";
+import SalutationSelectorInput from "../src/stories/Component/Inputs/SalutationSelectorInput";
+import TimePickerInput from "../src/stories/Component/Inputs/TimePickerInput";
 
 function App() {
   return (
     <div className="App">
-      <h1>Clean up for new styling</h1>
+      <h1>Verschiedene Inputfelder:</h1>
+      <BasicTextInput idName="firstName" title="Nachname" inputType="text" />
+      <br />
+      <BasicTextInput idName="password" title="Passwort" inputType="password" />
+      <br />
+      <SalutationSelectorInput />
+      <br />
+      <DatePickerInput />
+      <br />
+      <TimePickerInput />
     </div>
   );
 }
