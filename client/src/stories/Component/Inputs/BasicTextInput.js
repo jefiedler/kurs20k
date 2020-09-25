123b4e6c68d92b2fe663e9ab266c6b0ad60597f2
jefiedler <jonas.fiedler@magenta.de>
2020-09-25 17:20:54 +0200

    Add basic text input field

---
+++ client/src/stories/Component/Inputs/BasicTextInput.js
@@ -0,0 +1,41 @@
+import React from "react";
+import PropTypes from "prop-types";
+import TextField from "@material-ui/core/TextField";
+import { makeStyles } from "@material-ui/core";
+
+function BasicTextInput({ idName, title, inputType, onChange }) {
+  // const classes = useStyles();
+  return (
+    <>
+      <TextField
+        required
+        id={idName}
+        label={title}
+        type={inputType}
+        onChange={onChange}
+      />
+    </>
+  );
+}
+
+export default BasicTextInput;
+
+//Proptyps
+
+BasicTextInput.propTypes = {
+  idName: PropTypes.string,
+  title: PropTypes.string,
+  inputType: PropTypes.string,
+  onChange: PropTypes.func,
+};
+
+//Styling
+
+// const useStyles = makeStyles((theme) => ({
+//     root: {
+//       '& .MuiTextField-root': {
+//         margin: theme.spacing(1),
+//         width: '25ch',
+//       },
+//     },
+//   }));
