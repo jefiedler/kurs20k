47cc93f2b9b8455d0f386c03cfe780fad3eb008d
jefiedler <jonas.fiedler@magenta.de>
2020-09-25 15:38:47 +0200

    Add salutation selector input

---
+++ client/src/stories/Component/Inputs/SalutationSelectorInput.js
@@ -0,0 +1,53 @@
+import React from "react";
+import PropTypes from "prop-types";
+import { FormControl, InputLabel, MenuItem, Select } from "@material-ui/core";
+import { makeStyles } from "@material-ui/core/styles";
+
+function SalutationSelectorinput() {
+  const classes = useStyles();
+  const [salutationValue, setsalutaionValue] = React.useState("");
+
+  const handleChange = (event) => {
+    setsalutaionValue(event.target.value);
+  };
+
+  return (
+    <>
+      <FormControl className={classes.formControl}>
+        <InputLabel id="salutation-select-label">Anrede</InputLabel>
+        <Select
+          labelId="salutation-select-label"
+          id="salutaion-select"
+          value={salutationValue}
+          onChange={handleChange}
+        >
+          <MenuItem value={"Herr"}>Herr</MenuItem>
+          <MenuItem value={"Frau"}>Frau</MenuItem>
+        </Select>
+      </FormControl>
+    </>
+  );
+}
+
+export default SalutationSelectorinput;
+
+//PropTypes
+
+SalutationSelectorinput.propTypes = {};
+
+//Styles
+const useStyles = makeStyles((theme) => ({
+  root: {
+    "& .MuiTextField-root": {
+      margin: theme.spacing(1),
+      width: 200,
+    },
+  },
+  formControl: {
+    margin: theme.spacing(1),
+    minWidth: 120,
+  },
+  selectEmpty: {
+    marginTop: theme.spacing(2),
+  },
+}));
