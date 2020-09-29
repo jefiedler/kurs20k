a132def3276f2f98ce9b7200f8cb589dd297ee06
jefiedler <jonas.fiedler@magenta.de>
2020-09-29 10:51:40 +0200

    Add MuiPickersUtilsProvider to app.js https://material-ui-pickers.dev/getting-started/installation

--- client/src/App.js
+++ client/src/App.js
@@ -1,22 +1,13 @@
 import React from "react";
-import BasicTextInput from "../src/stories/Component/Inputs/BasicTextInput";
-import DatePickerInput from "../src/stories/Component/Inputs/DatePickerInput";
-import SalutationSelectorInput from "../src/stories/Component/Inputs/SalutationSelectorInput";
-import TimePickerInput from "../src/stories/Component/Inputs/TimePickerInput";
+import { MuiPickersUtilsProvider } from "@material-ui/pickers";
+import DateFnsUtils from "@date-io/date-fns";
 
 function App() {
   return (
     <div className="App">
-      <h1>Verschiedene Inputfelder:</h1>
-      <BasicTextInput idName="firstName" title="Nachname" inputType="text" />
-      <br />
-      <BasicTextInput idName="password" title="Passwort" inputType="password" />
-      <br />
-      <SalutationSelectorInput />
-      <br />
-      <DatePickerInput />
-      <br />
-      <TimePickerInput />
+      <MuiPickersUtilsProvider utils={DateFnsUtils}>
+        <h1>Clean up for new styling</h1>
+      </MuiPickersUtilsProvider>
     </div>
   );
 }
