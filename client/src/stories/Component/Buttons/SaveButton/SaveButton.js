8c0c5c30415cd418bcfd7c806227f894ef4f42c5
jefiedler <jonas.fiedler@magenta.de>
2020-09-29 15:05:21 +0200

    Add save button

---
+++ client/src/stories/Component/Buttons/SaveButton/SaveButton.js
@@ -0,0 +1,39 @@
+import React from "react";
+import Button from "@material-ui/core/Button";
+import { makeStyles } from "@material-ui/core/styles";
+import SaveIcon from "@material-ui/icons/Save";
+import PropTypes from "prop-types";
+
+function DeleteButton({ variant, color, size, handleClick }) {
+  const classes = useStyles();
+
+  return (
+    <Button
+      variant={variant}
+      color={color}
+      size={size}
+      onclick={handleClick}
+      className={classes.button}
+      startIcon={<SaveIcon />}
+    >
+      Löschen
+    </Button>
+  );
+}
+
+export default DeleteButton;
+
+//PropTypes
+DeleteButton.propTypes = {
+  variant: PropTypes.string,
+  color: PropTypes.string,
+  size: PropTypes.string,
+  handleClick: PropTypes.func,
+};
+
+//Styyling
+const useStyles = makeStyles((theme) => ({
+  button: {
+    margin: theme.spacing(1),
+  },
+}));
