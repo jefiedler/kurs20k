13df79d1af5be48b453871d9841c3026e6dca46c
jefiedler <jonas.fiedler@magenta.de>
2020-10-02 17:14:00 +0200

    Add show participant page

---
+++ client/src/pages/ShowParticipantPage.js
@@ -0,0 +1,72 @@
+import React from "react";
+import PropTypes from "prop-types";
+import styled from "@emotion/styled";
+import MainHeader from "../component/Headers/index";
+import NavBar from "../component/NavBars/NavBar";
+import MainFooter from "../component/Footer/index";
+import ContentShowParticipant from "../component/Content/Participant/Show";
+
+function ShowParticipantPage({ link, title, user, imgPath, imgTitle }) {
+  return (
+    <Container>
+      <HeaderContainer>
+        <MainHeader
+          href={link}
+          titleName={title}
+          userName={user}
+          imgSrc={imgPath}
+          imgName={imgTitle}
+        />
+      </HeaderContainer>
+      <NavContainer>
+        <NavBar />
+      </NavContainer>
+      <ContentContainer>
+        <ContentShowParticipant />
+      </ContentContainer>
+      <FooterContainer>
+        <MainFooter />
+      </FooterContainer>
+    </Container>
+  );
+}
+
+export default ShowParticipantPage;
+
+//Proptypes
+ShowParticipantPage.propTypes = {
+  link: PropTypes.string,
+  title: PropTypes.string,
+  user: PropTypes.string,
+  imgPath: PropTypes.string,
+  imgTitle: PropTypes.string,
+};
+
+//Styling
+
+const Container = styled.div`
+  display: grid;
+  grid-template-columns: repeat(16, 120px);
+  grid-template-rows: repeat(9, 120px);
+`;
+
+const HeaderContainer = styled.div`
+  grid-column: 1 / 16;
+  grid-row: 1 / 2;
+  z-index: 1;
+`;
+const NavContainer = styled.div`
+  grid-column: 1 / 3;
+  grid-row: 1 / 10;
+  z-index: 2;
+`;
+const ContentContainer = styled.div`
+  grid-column: 3 / 16;
+  grid-row: 2 / 9;
+`;
+
+const FooterContainer = styled.div`
+  grid-column: 1 / 16;
+  grid-row: 9 / 10;
+  z-index: 1;
+`;
